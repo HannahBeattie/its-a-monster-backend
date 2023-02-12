562f0f71bd5ef72908afb0ca4615a596c6d1d71d
Hannah Beattie <hannahdbeattie@gmail.com>
2023-02-12 13:13:15 +1300

    create datatype for worries and user, set user permissions

---
+++ src/api/worry/controllers/worry.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * worry controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::worry.worry');
---
+++ src/api/worry/routes/worry.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * worry router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::worry.worry');
